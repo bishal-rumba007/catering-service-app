ab7b936f81ee4627cb12bc82e6abf0b130c18b3e
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-12-09 22:41:17 +0545

    create text, button, icon themes

--- lib/src/themes/theme.dart
+++ lib/src/themes/theme.dart
@@ -2,157 +2,180 @@ import 'package:catering_service_app/src/themes/color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class AppTheme {
   ThemeData lightMode = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
     colorScheme: const ColorScheme.light(
-      background: Color(0xFFfbfbf2),
-      // background: Color.fromARGB(255, 240, 239, 232),
-      primary: AppColor.purpleColor,
-      secondary: AppColor.roseColor,
+      background: AppColor.backGroundColor,
+      primary: AppColor.primaryRed,
+      secondary: AppColor.secondaryColor,
+    ),
+    iconTheme: const IconThemeData(
+      color: AppColor.primaryRed,
+    ),
+    textTheme: TextTheme(
+      displayLarge: GoogleFonts.inter(
+        fontSize: 32,
+        fontWeight: FontWeight.w900,
+        color: AppColor.darkColor,
+      ),
+      displayMedium: GoogleFonts.inter(
+        fontSize: 57,
+        fontWeight: FontWeight.w400,
+        color: AppColor.darkColor,
+      ),
+      displaySmall: GoogleFonts.inter(
+        fontSize: 36,
+        fontWeight: FontWeight.w400,
+        color: AppColor.darkColor,
+      ),
+      headlineLarge: GoogleFonts.inter(
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: AppColor.darkColor,
+      ),
+      headlineMedium: GoogleFonts.inter(
+        fontSize: 28,
+        fontWeight: FontWeight.w400,
+        color: AppColor.darkColor,
+      ),
+      headlineSmall: GoogleFonts.inter(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: AppColor.darkColor,
+      ),
+      titleLarge: GoogleFonts.inter(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: AppColor.darkColor,
+      ),
+      titleMedium: GoogleFonts.inter(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor,
+      ),
+      titleSmall: GoogleFonts.inter(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor,
+      ),
+      bodyLarge: GoogleFonts.inter(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: AppColor.darkColor,
+      ),
+      bodyMedium: GoogleFonts.inter(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor.withOpacity(0.5),
+      ),
+      bodySmall: GoogleFonts.inter(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: AppColor.darkColor,
+      ),
+      labelLarge: GoogleFonts.inter(
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+        color: AppColor.darkColor,
+      ),
+      labelMedium: GoogleFonts.inter(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor,
+      ),
+      labelSmall: GoogleFonts.inter(
+        fontSize: 11,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor,
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return AppColor.greyColor.withOpacity(0.5);
+            }
+            return Colors.transparent;
+          },
+        ),
+        textStyle: MaterialStateProperty.all<TextStyle>(
+          GoogleFonts.inter(
+            fontWeight: FontWeight.w800,
+            fontSize: 16,
+            color: AppColor.darkColor,
+          ),
+        ),
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+        ),
+        foregroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.disabled)) {
+              return Colors.grey;
+            }
+            return Colors.white; // Customize the button text color
+          },
+        ),
+        side: MaterialStateProperty.all<BorderSide>(
+          const BorderSide(
+            color: AppColor.greyColor,
+            width: 1,
+          ),
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(AppColor.primaryRed),
+        textStyle: MaterialStateProperty.all<TextStyle>(
+          GoogleFonts.inter(
+            fontWeight: FontWeight.w800,
+            fontSize: 16,
+            color: Colors.white, // Text color for the button
+          ),
+        ),
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding:
+          const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: const BorderSide(color: AppColor.greyColor, width: 1.0),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: const BorderSide(color: AppColor.greyColor, width: 1.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: const BorderSide(color: AppColor.primaryRed, width: 2.0),
+      ),
+      labelStyle: GoogleFonts.inter(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: AppColor.darkColor.withOpacity(0.5),
+      ),
+      errorStyle: GoogleFonts.inter(
+        fontSize: 12,
+        color: Colors.red,
+      ),
     ),
   );
-
-  ThemeData darkMode = ThemeData(
-      useMaterial3: true,
-      brightness: Brightness.dark,
-      colorScheme: const ColorScheme.dark(
-        background: AppColor.darkColor,
-        primary: AppColor.purpleColor,
-        secondary: AppColor.roseColor,
-      ),
-      textTheme: TextTheme(
-        displayLarge: GoogleFonts.raleway(
-          fontSize: 45,
-          fontWeight: FontWeight.w900,
-          color: Colors.white,
-        ),
-        displayMedium: GoogleFonts.raleway(
-          fontSize: 57,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
-        ),
-        displaySmall: GoogleFonts.raleway(
-          fontSize: 36,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
-        ),
-        headlineLarge: GoogleFonts.raleway(
-          fontSize: 32,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
-        ),
-        headlineMedium: GoogleFonts.raleway(
-          fontSize: 28,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
-        ),
-        headlineSmall: GoogleFonts.raleway(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-        titleLarge: GoogleFonts.raleway(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-        titleMedium: GoogleFonts.raleway(
-          fontSize: 16,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-        ),
-        titleSmall: GoogleFonts.raleway(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-        ),
-        bodyLarge: GoogleFonts.raleway(
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
-          color: Colors.white,
-        ),
-        bodyMedium: GoogleFonts.raleway(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
-          color: Colors.white.withOpacity(0.5),
-        ),
-        bodySmall: GoogleFonts.raleway(
-          fontSize: 12,
-          fontWeight: FontWeight.w400,
-          color: Colors.white,
-        ),
-        labelLarge: GoogleFonts.raleway(
-          fontSize: 14,
-          fontWeight: FontWeight.w600,
-          color: Colors.white,
-        ),
-        labelMedium: GoogleFonts.raleway(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-        ),
-        labelSmall: GoogleFonts.raleway(
-          fontSize: 11,
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-        ),
-      ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.resolveWith<Color>(
-            (Set<MaterialState> states) {
-              if (states.contains(MaterialState.disabled)) {
-                return AppColor.darkColor.withOpacity(0.3);
-              }
-              return Colors.transparent; // Customize the button color
-            },
-          ),
-          textStyle: MaterialStateProperty.all<TextStyle>(
-            GoogleFonts.raleway(
-              fontWeight: FontWeight.w600,
-              fontSize: 14,
-              color: AppColor.purpleColor.withOpacity(0.5),
-            ),
-          ),
-          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-            const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-          ),
-          foregroundColor: MaterialStateProperty.resolveWith<Color>(
-            (Set<MaterialState> states) {
-              if (states.contains(MaterialState.disabled)) {
-                return Colors.grey;
-              }
-              return Colors.white; // Customize the button text color
-            },
-          ),
-          side: MaterialStateProperty.all<BorderSide>(
-            BorderSide(
-              color: AppColor.purpleColor.withOpacity(0.5),
-              width: 1,
-            ),
-          ),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                  8.0), // Adjust the borderRadius value as needed
-            ),
-          ),
-        ),
-      ),
-      chipTheme: ChipThemeData(
-        backgroundColor: AppColor.purpleColor.withOpacity(0.1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-        side: BorderSide.none,
-        labelStyle: GoogleFonts.raleway(
-          fontSize: 12,
-          fontWeight: FontWeight.w700,
-          color: AppColor.purpleColor.withOpacity(0.7),
-        ),
-        labelPadding: const EdgeInsets.symmetric(horizontal: 4, vertical: 1),
-      ));
 }
