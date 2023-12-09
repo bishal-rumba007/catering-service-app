8483738a727721936b7b43832f70fd0490b278fa
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-12-09 22:41:45 +0545

    create color pallete

--- lib/src/themes/color.dart
+++ lib/src/themes/color.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color darkColor = Color(0xFF090E16);
-  static const Color purpleColor = Color(0xFF7B4AE2);
-  static const Color roseColor = Color(0xFFC83267);
-  static const Color avatarBackground = Color(0xFF84868A);
+  static const Color primaryRed = Color(0xFFf05242);
+  static const Color secondaryColor = Color(0xFF4285F4);
+  static const Color backGroundColor = Color(0xFFf2f2f3);
+  static const Color darkColor = Color(0xFF252425);
+  static const Color greyColor = Color(0xFF636363);
+  static const Color grey2Color = Color(0xFFd8d8d9);
+  static const Color containerColor = Color(0xFFfbd652);
+  static const Color meanGreen = Color(0xFF42861d);
+  static const Color setOrgane = Color(0xFFf58830);
 }
