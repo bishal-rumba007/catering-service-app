8f80a8abee9a7c1e5c9a6df5164cb286b700ee21
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-03 22:19:54 +0545

    add provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:catering_service_app/src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'firebase_options.dart';
+import 'src/firebase/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
