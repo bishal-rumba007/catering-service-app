3e2d452c44be9eb5595184d2df222f03f19cd7c7
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-03 22:21:01 +0545

    update login page

--- lib/src/features/auth/screens/login_screen.dart
+++ lib/src/features/auth/screens/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:catering_service_app/src/common/common_export.dart';
+import 'package:catering_service_app/src/features/auth/screens/sign_up_screen.dart';
+import 'package:catering_service_app/src/themes/export_themes.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,6 +11,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,48 +21,63 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              Text(
-                'Login',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge
-                    ?.copyWith(color: Colors.black),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  label: Text('Username'),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 200,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                  suffixIcon: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.remove_red_eye_rounded),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Login',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(color: Colors.black),
                   ),
                 ),
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Login'),
-              ),
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+                BuildTextFormField(
+                  controller: _usernameController,
+                  labelText: 'Username',
+                  textInputType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BuildPasswordTextFormField(
+                  labelText: 'Password',
+                  textInputAction: TextInputAction.done,
+                  controller: _passwordController,
+                ),
+                const SizedBox(height: 50),
+                BuildButton(
+                  onPressed: (){
+
+                  },
+                  buttonWidget: const Text('Login'),
+                ),
+                const SizedBox(height: 60,),
+                Text(
+                  'Don\'t have an account?',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: AppColor.darkColor.withOpacity(0.9),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => const SignUpScreen(),));
+                  },
+                  child: const Text('Create an account'),
+                )
+              ],
+            ),
           ),
         ),
       ),
