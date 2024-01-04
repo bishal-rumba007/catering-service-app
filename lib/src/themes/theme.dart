f0a5ef5f719310807bbf923ceeeacea117e10d34
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-04 15:27:40 +0545

    add responsiveness

--- lib/src/themes/theme.dart
+++ lib/src/themes/theme.dart
@@ -1,5 +1,6 @@
 import 'package:catering_service_app/src/themes/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
@@ -16,77 +17,77 @@ class AppTheme {
     ),
     textTheme: TextTheme(
       displayLarge: GoogleFonts.inter(
-        fontSize: 32,
+        fontSize: 42.sp,
         fontWeight: FontWeight.w900,
         color: AppColor.darkColor,
       ),
       displayMedium: GoogleFonts.inter(
-        fontSize: 57,
+        fontSize: 40.sp,
         fontWeight: FontWeight.w400,
         color: AppColor.darkColor,
       ),
       displaySmall: GoogleFonts.inter(
-        fontSize: 36,
+        fontSize: 36.sp,
         fontWeight: FontWeight.w400,
         color: AppColor.darkColor,
       ),
       headlineLarge: GoogleFonts.inter(
-        fontSize: 32,
+        fontSize: 32.sp,
         fontWeight: FontWeight.w700,
         color: AppColor.darkColor,
       ),
       headlineMedium: GoogleFonts.inter(
-        fontSize: 28,
+        fontSize: 28.sp,
         fontWeight: FontWeight.w400,
         color: AppColor.darkColor,
       ),
       headlineSmall: GoogleFonts.inter(
-        fontSize: 24,
+        fontSize: 24.sp,
         fontWeight: FontWeight.bold,
         color: AppColor.darkColor,
       ),
       titleLarge: GoogleFonts.inter(
-        fontSize: 20,
+        fontSize: 20.sp,
         fontWeight: FontWeight.bold,
         color: AppColor.darkColor,
       ),
       titleMedium: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor,
       ),
       titleSmall: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor,
       ),
       bodyLarge: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         fontWeight: FontWeight.w600,
         color: AppColor.darkColor,
       ),
       bodyMedium: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor.withOpacity(0.7),
       ),
       bodySmall: GoogleFonts.inter(
-        fontSize: 12,
+        fontSize: 12.sp,
         fontWeight: FontWeight.w400,
         color: AppColor.darkColor,
       ),
       labelLarge: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w600,
         color: AppColor.darkColor,
       ),
       labelMedium: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor,
       ),
       labelSmall: GoogleFonts.inter(
-        fontSize: 11,
+        fontSize: 11.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor,
       ),
@@ -134,57 +135,57 @@ class AppTheme {
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(double.infinity, 50),
+        minimumSize: Size(double.infinity, 50.h),
         textStyle:  GoogleFonts.inter(
           fontWeight: FontWeight.w600,
-          fontSize: 16,
+          fontSize: 16.sp,
           color: Colors.white,
         ),
         backgroundColor: AppColor.primaryRed,
         foregroundColor: Colors.white,
         elevation: 2,
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
       )
     ),
 
     inputDecorationTheme: InputDecorationTheme(
       contentPadding:
-          const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+          EdgeInsets.symmetric(vertical: 14.h, horizontal: 16.w),
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: BorderRadius.circular(8.r),
         borderSide: const BorderSide(color: AppColor.greyColor, width: 1.0),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: BorderRadius.circular(8.r),
         borderSide: BorderSide(color: AppColor.greyColor.withOpacity(0.8), width: 1.0),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: BorderRadius.circular(8.r),
         borderSide: const BorderSide(color: AppColor.primaryRed, width: 1.5),
       ),
       labelStyle: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w500,
         color: AppColor.darkColor.withOpacity(0.6),
       ),
       floatingLabelStyle: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         fontWeight: FontWeight.w600,
         color: AppColor.primaryRed,
       ),
       errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8.0),
+        borderRadius: BorderRadius.circular(8.r),
         borderSide: const BorderSide(color: AppColor.setOrange, width: 1.5),
       ),
       errorStyle: GoogleFonts.inter(
-        fontSize: 12,
+        fontSize: 14.sp,
         color: Colors.red,
       ),
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
         textStyle: GoogleFonts.inter(
-          fontSize: 14,
+          fontSize: 14.sp,
           fontWeight: FontWeight.w600
         )
       )
