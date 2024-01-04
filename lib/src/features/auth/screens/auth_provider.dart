d814d9374fc4f25119ad7e364ffb979c2cd7e987
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-04 15:29:43 +0545

    create authentication stream

--- lib/src/features/auth/screens/auth_provider.dart
+++ lib/src/features/auth/screens/auth_provider.dart
@@ -3,8 +3,11 @@
 
 import 'package:catering_service_app/src/features/auth/data/auth_repo_impl.dart';
 import 'package:catering_service_app/src/features/auth/domain/auth_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authProvider = Provider<AuthRepository>((ref) {
   return AuthRepoImpl();
 });
+
+final authStream = StreamProvider.autoDispose((ref) => FirebaseAuth.instance.authStateChanges());
