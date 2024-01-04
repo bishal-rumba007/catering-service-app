f10dbdf6ca8877f8255cf2660885ed847218fc85
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-04 15:30:02 +0545

    create main screen

--- lib/src/features/dashboard/screens/main_screen.dart
+++ lib/src/features/dashboard/screens/main_screen.dart
@@ -1,21 +1,32 @@
+import 'package:catering_service_app/src/features/auth/screens/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class MainScreen extends ConsumerStatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<MainScreen> createState() => _MainScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _MainScreenState extends ConsumerState<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    final authData = ref.watch(authProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () async{
+              await authData.logOut();
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
       ),
     );
   }
