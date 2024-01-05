b08ec7c3b12b5d3d4d88abeb0aaba3f98ba147c8
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-05 22:17:10 +0545

    change route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(428, 926),
+      designSize: const Size(375, 812),
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
