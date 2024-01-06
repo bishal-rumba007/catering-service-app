e10ae0b540b761a1595de5f53347930ca94aff26
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-06 20:43:12 +0545

    add category section/ code refactor

--- lib/src/features/dashboard/screens/home_screen.dart
+++ lib/src/features/dashboard/screens/home_screen.dart
@@ -15,6 +15,12 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int selectedCategory = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final authData = ref.watch(authProvider);
@@ -66,7 +72,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
               onPressed: () async{
                 await authData.logOut();
               },
-              icon: Icon(Icons.logout),
+              icon: const Icon(Icons.logout),
             )
           ],
         ),
@@ -74,74 +80,162 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 18.w),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20.h,),
-            Card(
-              color: Colors.yellow,
-              elevation: 3,
-              shadowColor: Colors.black.withOpacity(0.55),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
-                height: 130.h,
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: '30% OFF',
-                                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                                  fontSize: 17.sp
-                                ),
-                              ),
-                              TextSpan(
-                                text: ' on all \n categories',
-                                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                                  fontSize: 15.sp,
-                                  fontWeight: FontWeight.w600
-                                ),
-                              )
-                            ]
-                          ),
-                        ),
-                        SizedBox(height: 12.h,),
-                        SizedBox(
-                          width: 104.w,
-                          height: 32.h,
-                          child: BuildButton(
-                            onPressed: (){},
-                            buttonStyle: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red,
-                              padding: EdgeInsets.all(5.w),
-                              textStyle: Theme.of(context).textTheme.bodySmall?.copyWith(
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white
-                              ),
+            const CarouselCard(),
+            SizedBox(height: 20.h,),
+            Text(
+              'Choose Your Categories',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                fontSize: 18.sp
+              ),
+            ),
+            SizedBox(height: 10.h,),
+            buildCategories(),
+            SizedBox(height: 20.h,),
+            Text(
+              'Hello Bishal Rumba',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                fontSize: 18.sp,
+                color: AppColor.greyColor,
+              ),
+            ),
+            Text(
+              'Planning A Fabulous Event?',
+              style: Theme.of(context).textTheme.headlineSmall
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildCategories() {
+    return SizedBox(
+            height: 90.h,
+            width: double.infinity,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedCategory = index;
+                    });
+                    // Todo: add routing logic here
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Card(
+                        margin: EdgeInsets.zero,
+                        elevation: 3.0,
+                        child: Container(
+                          height: 62.h,
+                          width: 60.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.r),
+                            image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhjY_J2A-6p8NNQGmCLfsSbjR8RaRinc1p2g")
                             ),
-                            buttonWidget: const Text('Accept Now',),
+                            border: index == selectedCategory ? Border.all(
+                              color: AppColor.primaryRed,
+                              width: 1.8.w,
+                            ) : const Border()
                           ),
-                        )
-                      ],
-                    ),
-                    SizedBox(width: 34.w,),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.r),
-                          image: const DecorationImage(
-                            image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhjY_J2A-6p8NNQGmCLfsSbjR8RaRinc1p2g",),
-                            fit: BoxFit.cover,
-                          )
                         ),
                       ),
+                      const Spacer(),
+                      Text(
+                        'Wedding',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 14.sp,
+                          color: index == selectedCategory ? AppColor.primaryRed : AppColor.greyColor
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(width: 15.w,),
+            ),
+          );
+  }
+}
+
+class CarouselCard extends StatelessWidget {
+  const CarouselCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.yellow,
+      elevation: 3,
+      shadowColor: Colors.black.withOpacity(0.55),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+        height: 130.h,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: '30% OFF',
+                        style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          fontSize: 17.sp
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' on all \n categories',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w600
+                        ),
+                      )
+                    ]
+                  ),
+                ),
+                SizedBox(height: 12.h,),
+                SizedBox(
+                  width: 104.w,
+                  height: 32.h,
+                  child: BuildButton(
+                    onPressed: (){},
+                    buttonStyle: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: EdgeInsets.all(5.w),
+                      textStyle: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white
+                      ),
                     ),
-                  ],
+                    buttonWidget: const Text('Accept Now',),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(width: 34.w,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.r),
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhjY_J2A-6p8NNQGmCLfsSbjR8RaRinc1p2g",),
+                  )
                 ),
               ),
             ),
