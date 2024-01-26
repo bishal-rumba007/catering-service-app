5a1d8842e6b99653a37d63cb7c1837355eaa9143
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-26 23:51:47 +0545

    create order status method

--- lib/src/features/order/data/order_datasource.dart
+++ lib/src/features/order/data/order_datasource.dart
@@ -11,20 +11,28 @@ class OrderDataSource{
   final _orderDb = FirebaseFirestore.instance.collection('orders');
 
 
-  Future<List<OrderModel>> getOrders() async{
-    try{
-      final userData = await _userDb.doc(uid).get();
-      final userId = userData.id;
-      final response = await _orderDb
-          .where('catererId', isEqualTo: userId)
-          .get();
-      final orderList = await Future.wait(response.docs.map((doc) async {
-        final json = doc.data();
-        final categoryImage = await getCategoryImage(json['categoryId']);
-        return OrderModel.fromJson({...json, 'categoryImage': categoryImage});
-      }));
-      return orderList;
-    }on FirebaseException catch(error){
+  Stream<List<OrderModel>> getOrdersStream() {
+    try {
+      return _userDb.doc(uid).snapshots().asyncMap((userData) async {
+        final userId = userData.id;
+
+        final response = await _orderDb
+            .where('catererId', isEqualTo: userId)
+            .get();
+
+        final orderList = await Future.wait(response.docs.map((doc) async {
+          final json = doc.data();
+          final categoryImage = await getCategoryImage(json['categoryId']);
+          return OrderModel.fromJson({
+            ...json,
+            'orderId': doc.id,
+            'categoryImage': categoryImage,
+          });
+        }));
+
+        return orderList;
+      });
+    } on FirebaseException catch (error) {
       throw '$error';
     }
   }
@@ -44,19 +52,37 @@ class OrderDataSource{
     }
   }
 
-  // Future<Menus?> getMenuInfo(String menuId) async {
-  //   try {
-  //     final menuSnapshot = await _menuDb.doc(menuId).get();
-  //     if (menuSnapshot.exists) {
-  //       final json = menuSnapshot.data();
-  //       return Menus.fromJson(json!);
-  //     } else {
-  //       return null;
-  //     }
-  //   } on FirebaseException catch (err) {
-  //     throw '$err';
-  //   }
-  // }
+  Future<String> acceptOrder({required String orderId}) async {
+    try {
+      await _orderDb.doc(orderId).update({
+        'orderStatus': OrderStatus.accepted.index,
+      });
+      return 'Order accepted';
+    } on FirebaseException catch (err) {
+      throw '$err';
+    }
+  }
 
+  Future<String> completeOrder({required String orderId}) async {
+    try {
+      await _orderDb.doc(orderId).update({
+        'orderStatus': OrderStatus.completed.index,
+      });
+      return 'Order completed';
+    } on FirebaseException catch (err) {
+      throw '$err';
+    }
+  }
+
+  Future<String> rejectOrder({required String orderId}) async {
+    try {
+      await _orderDb.doc(orderId).update({
+        'orderStatus': OrderStatus.rejected.index,
+      });
+      return 'Order rejected';
+    } on FirebaseException catch (err) {
+      throw '$err';
+    }
+  }
 
 }
