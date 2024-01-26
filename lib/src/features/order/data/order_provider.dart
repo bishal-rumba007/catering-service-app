48afaffc74bbe11c7fe176c56113bc01d5b1eee3
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-01-26 23:52:20 +0545

    update order provider, create status providers

--- lib/src/features/order/data/order_provider.dart
+++ lib/src/features/order/data/order_provider.dart
@@ -6,4 +6,17 @@ import 'package:catering_service_app/src/features/order/data/order_datasource.da
 import 'package:catering_service_app/src/features/order/domain/models/order_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final orderProvider = FutureProvider<List<OrderModel>>((ref) => OrderDataSource().getOrders());
+final orderProvider = StreamProvider<List<OrderModel>>((ref) => OrderDataSource().getOrdersStream());
+
+final acceptOrderProvider = FutureProvider.family<String, String>(
+        (ref, String orderId) => OrderDataSource().acceptOrder(orderId: orderId)
+);
+
+
+final completeOrderProvider = FutureProvider.family<String, String>(
+        (ref, String orderId) => OrderDataSource().completeOrder(orderId: orderId)
+);
+
+final rejectOrderProvider = FutureProvider.family<String, String>(
+        (ref, String orderId) => OrderDataSource().rejectOrder(orderId: orderId)
+);
