0817a06830e62030e9a560531d9cc0b1a79ff546
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-02-08 16:30:36 +0545

    ignore api keys

--- lib/src/api/endpoints.dart
+++
@@ -1,7 +0,0 @@
-
-
-
-class ApiEndPoints{
-  static const String baseNotificationUrl = 'https://fcm.googleapis.com/fcm/send';
-  static const String notificationUrl = 'https://fcm.googleapis.com/v1/projects/cateringservicemgmt/messages:send';
-}
